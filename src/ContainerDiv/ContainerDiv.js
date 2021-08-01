70e571bcbed1b2e3c5bf9eb699b3a6014adb529e
David John <david.john@mail.utoronto.ca>
2021-08-01 16:31:34 -0400

    Added additional dependencies including parallax and debug

---
+++ src/ContainerDiv/ContainerDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ContainerDiv = () => {
+  return (
+    <section class="pg3LgContainer lgContainerDiv">
+      <div class="container-fluid noFilt lgContainerInit transition1">
+        <div class="sideWipeContainer">
+          <div class="sideWipe1 leftSideWipe"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContainerDiv;
